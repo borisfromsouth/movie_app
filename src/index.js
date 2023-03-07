8265d9c6d00b48386cdad720a5e602b0a131fb8e
boris_from_south <sashaborisenko@tut.by>
2023-03-07 17:00:45 +0300

    Acquaintance with code
    Observer project structure and clean useless components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+//  рендер результата функции App, результат возвращается в public -> index, реакт загружает основной файл index, рендерит остальные элементы и пропушивает их на места в разметке страницы
+//  <App /> - компонент, по сути функция которая возвращает html-код
+//  Формат  <App /> - JSX
+root.render(        
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
